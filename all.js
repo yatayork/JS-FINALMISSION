831f696f9223410b8d5e71a64b13c273190f4108
Yatayork <103381337+yatayork@users.noreply.github.com>
2023-09-23 14:39:59 +0800

    update

--- all.js
+++ all.js
@@ -14,23 +14,23 @@
 //資料提取處(data)
 let data = [
     {
-        content: "test",
+        content: "1",
         checked: "false",
     },
     {
-        content: "test",
+        content: "2",
         checked: "false",
     },
     {
-        content: "test",
+        content: "3",
         checked: "false",
     },
     {
-        content: "test",
+        content: "4",
         checked: "false",
     },
     {
-        content: "test",
+        content: "5",
         checked: "false",
     },
 ];
@@ -46,6 +46,23 @@ const filter = document.querySelector(".filter");
 const all = document.querySelector(".allThing");
 const undone = document.querySelector(".undone");
 const done = document.querySelector(".done");
+
+const footer = document.querySelector(".footer");
+const clearCheck = document.querySelector(".clearCheck");
+const undone_item = document.querySelector(".undone-item");
+
+function renderItem() {
+    let num = 0;
+    data.forEach(function (item, index) {
+        if (item.checked == "false") {
+            num += 1;
+        } else {
+            num -= 0;
+        }
+    });
+    // console.log(num);
+    undone_item.textContent = `${num}個待完成項目`;
+}
 
 function renderData() {
     let str = ""; //用來組字串的放置區
@@ -64,7 +81,10 @@ function renderData() {
     list.innerHTML = str; //對list區塊 放入那些li跟按鈕來達成待辦事項新增
 }
 
+renderItem();
 renderData();
+
+// renderItem();
 //新增功能
 save.addEventListener("click", function (e) {
     if (txt.value == "") {
@@ -75,11 +95,13 @@ save.addEventListener("click", function (e) {
     // console.log(txt.value);
     let obj = {}; //用來放置value的區域
     obj.content = txt.value;
+    obj.checked = "false";
     //obj.content的意思是原本為空物件的obj裡新增一個名為content的屬性(跟我們的data一樣) 將value帶進去
     data.push(obj); //因為是照著順序排列所以只要透過push()放到最後一個就好
     txt.value = ""; //這個動作是將已經push完的自己打的值清除 讓填寫欄變乾淨等待下一次填寫，提升使用者體驗
 
     renderData(); //初始化一次將新資料帶入畫面
+    renderItem();
 });
 
 //刪除功能
@@ -91,6 +113,7 @@ list.addEventListener("click", function (e) {
     //抽取該按鈕被放入的data-num
     data.splice(num, 1); //清除該項目
     renderData(); //在重新初始化刪除過後的renderData()裡的str讓資料顯示
+    renderItem();
 });
 
 list.addEventListener("click", (e) => {
@@ -99,38 +122,25 @@ list.addEventListener("click", (e) => {
     }
     const check = e.target.checked;
     //確認checkbox是否被點擊 true or false
-    console.log(check);
+    // console.log(check);
     //在裡面設置data-num針對單一項目進行checked屬性的切換
     //為後續待完成已完成頁面做篩選用途
     let num = e.target.getAttribute("data-num");
-    console.log(num);
+    // console.log(num);
     if (data[e.target.getAttribute("data-num")].checked === "false") {
         data[e.target.getAttribute("data-num")].checked = "true";
+        renderItem();
     } else {
         data[e.target.getAttribute("data-num")].checked = "false";
+        renderItem();
     }
 });
 
 filter.addEventListener("click", function (e) {
-    let str = ""; //用來組字串的放置區
-    data.forEach(function (item, index) {
-        //依照data內每個{}帶入他們的content(這邊只有單項),跟索引index
-        str += `<li class="flex">
-                <label class="checkbox" for="">
-                    <input type="checkbox" class="check" data-num=${index} />
-                    <span>${item.content}</span>
-                </label>
-                <a href="#" class="delete" >
-                    <span data-name="delete" data-num=${index} class="material-icons"> clear </span>
-                </a>
-            </li>`;
-    });
-    list.innerHTML = str; //對list區塊 放入那些li跟按鈕來達成待辦事項新增
+    renderData();
 });
 
 filter.addEventListener("click", function (e) {
-    console.log(e.target.getAttribute("class"));
-
     if (e.target.getAttribute("class") !== "undone") {
         return;
     }
@@ -152,8 +162,6 @@ filter.addEventListener("click", function (e) {
 });
 
 filter.addEventListener("click", function (e) {
-    console.log(e.target.getAttribute("class"));
-
     if (e.target.getAttribute("class") !== "done") {
         return;
     }
@@ -172,4 +180,21 @@ filter.addEventListener("click", function (e) {
         }
     });
     list.innerHTML = str;
+});
+
+const delete_item = document.querySelector("delete");
+
+footer.addEventListener("click", function (e) {
+    console.log(e.target.getAttribute("class"));
+    if (e.target.getAttribute("class") !== "clearCheck") {
+        return;
+    }
+    data.forEach(function (item, index) {
+        if (item.checked == "true") {
+            console.log("刪除");
+            console.log(index);
+            data.splice(index, 1);
+        }
+    });
+    renderData();
 });
